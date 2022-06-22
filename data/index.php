e8ac0fcc4e4f02f71ce5123b2b96ea383d2f9aa6
Amin Al Ali Al Darwish <amin@bunnyshell.com>
2022-06-22 15:10:38 +0100

    udpate

--- data/index.php
+++ data/index.php
@@ -17,79 +17,143 @@ $db->setTimeout(1.5);      // 1500 ms
 $db->setTimeout(10);       // 10 seconds
 $db->setConnectTimeOut(5); // 5 seconds
 
+
+// ---------------------------- Write ----------------------------
+echo "\n-----\ntry write:create_table\n";
+$db->database('default');
+//------------------------------------------------------------------------------
+
+echo 'Tables EXISTS: ' . json_encode($db->showTables()) . PHP_EOL;
+$db->write('DROP TABLE IF EXISTS summing_url_views');
+echo 'Tables EXISTS: ' . json_encode($db->showTables()) . PHP_EOL;
+
 $db->write('
     CREATE TABLE IF NOT EXISTS summing_url_views (
         event_date Date DEFAULT toDate(event_time),
         event_time DateTime,
+        url_hash String,
         site_id Int32,
-        site_key String,
         views Int32,
         v_00 Int32,
         v_55 Int32
-    )
-    ENGINE = SummingMergeTree(event_date, (site_id, site_key, event_time, event_date), 8192)
-');
+    ) 
+    ENGINE = SummingMergeTree(event_date, (site_id, url_hash, event_time, event_date), 8192)
+'
+);
+echo 'Table EXISTS: ' . json_encode($db->showTables()) . PHP_EOL;
 
-print_r($db->showTables());
-echo $db->showCreateTable('summing_url_views');
+/*
+Table EXISTS: [{"name": "summing_url_views"}]
+*/
+
+//------------------------------------------------------------------------------
+echo "Insert\n";
+
 $stat = $db->insert('summing_url_views',
     [
         [time(), 'HASH1', 2345, 22, 20, 2],
-        [time(), 'HASH2', 2345, 12, 9,  3],
+        [time(), 'HASH2', 2345, 12, 9, 3],
         [time(), 'HASH3', 5345, 33, 33, 0],
         [time(), 'HASH3', 5345, 55, 0, 55],
     ],
-    ['event_time', 'site_key', 'site_id', 'views', 'v_00', 'v_55']
+    ['event_time', 'url_hash', 'site_id', 'views', 'v_00', 'v_55']
 );
 
-$statement = $db->select('SELECT * FROM summing_url_views LIMIT 3');
+echo "Insert Done\n";
+//------------------------------------------------------------------------------
 
-// Count select rows
-$statement->count();
 
-// Count all rows
-$statement->countAll();
 
-// fetch one row
-$statement->fetchOne();
+echo "Try select \n";
 
-// get extremes min
-print_r($statement->extremesMin());
 
-// totals row
-print_r($statement->totals());
+$st = $db->select('SELECT * FROM summing_url_views LIMIT 2');
 
-// result all
-print_r($statement->rows());
 
-// totalTimeRequest
-print_r($statement->totalTimeRequest());
 
-// raw answer JsonDecode array, for economy memory
-print_r($statement->rawData());
 
-// raw curl_info answer
-print_r($statement->responseInfo());
+echo "Count select rows:".$st->count()."\n";
+echo "Count all rows:".$st->countAll()."\n";
+echo "First row:\n";
+print_r($st->fetchOne());
 
-// human size info
-print_r($statement->info());
+echo "extremes_min:\n";
+print_r($st->extremesMin());
 
-// if clickhouse-server version >= 54011
-$db->settings()->set('output_format_write_statistics',true);
-print_r($statement->statistics());
+echo "totals:\n";
+print_r($st->totals());
 
-$statement = $db->select('
-    SELECT event_date, site_key, sum(views), avg(views)
-    FROM summing_url_views
-    WHERE site_id < 3333
-    GROUP BY event_date, url_hash
-    WITH TOTALS
-');
 
-print_r($statement->rowsAsTree('event_date.site_key'));
 
-$db->write('DROP TABLE IF EXISTS summing_url_views');
-echo " \n deleted database";
+$st=$db->select('SELECT event_date,url_hash,sum(views),avg(views) FROM summing_url_views WHERE site_id<3333 GROUP BY event_date,url_hash WITH TOTALS');
+
+
+
+
+echo "Count select rows:".$st->count()."\n";
+/*
+2
+ */
+echo "Count all rows:".$st->countAll()."\n";
+/*
+false
+ */
+
+
+
+echo "First row:\n";
+print_r($st->fetchOne());
+/*
+(
+    [event_date] => 2016-07-18
+    [url_hash] => HASH1
+    [sum(views)] => 22
+    [avg(views)] => 22
+)
+ */
+
+
+echo "totals:\n";
+print_r($st->totals());
+/*
+(
+    [event_date] => 0000-00-00
+    [url_hash] =>
+    [sum(views)] => 34
+    [avg(views)] => 17
+)
+
+ */
+
+
+echo "Tree Path [event_date.url_hash]:\n";
+print_r($st->rowsAsTree('event_date.url_hash'));
+/*
+(
+    [2016-07-18] => Array
+        (
+            [HASH2] => Array
+                (
+                    [event_date] => 2016-07-18
+                    [url_hash] => HASH2
+                    [sum(views)] => 12
+                    [avg(views)] => 12
+                )
+            [HASH1] => Array
+                (
+                    [event_date] => 2016-07-18
+                    [url_hash] => HASH1
+                    [sum(views)] => 22
+                    [avg(views)] => 22
+                )
+        )
+)
+ */
+$db->write("DROP TABLE IF EXISTS summing_url_views");
+echo "Tables EXISTS:".json_encode($db->showTables())."\n";
+/*
+Tables EXISTS:[]
+ */
 
 
 if (!$db->ping()) {echo 'Error connect';} else { echo "\n Connected!";};
--- data/vendor/smi2/phpclickhouse/example/00_config_connect.php
+++ data/vendor/smi2/phpclickhouse/example/00_config_connect.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'host' => '127.0.0.1', // you hot name
+    'host' => $_ENV['CH_HOST'], // you hot name
     'port' => '8123',
     'username' => 'default',
     'password' => '',
