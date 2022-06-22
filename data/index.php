0478d1e2148336ecdb4ed23a10ee198b22d07495
Amin Al Ali Al Darwish <amin@bunnyshell.com>
2022-06-22 13:44:56 +0100

    first commit

---
+++ data/index.php
@@ -0,0 +1,22 @@
+<?php 
+
+require 'vendor/autoload.php';
+
+$_ENV['CH_HOST'] = 'clickhouse-5t7paj'
+
+$config = [
+    'host' => $_ENV['CH_HOST'],
+    'port' => '8123',
+    'username' => 'default',
+    'password' => ''
+];
+
+$db = new ClickHouseDB\Client($config);
+$db->database('default');
+$db->setTimeout(1.5);      // 1500 ms
+$db->setTimeout(10);       // 10 seconds
+$db->setConnectTimeOut(5); // 5 seconds
+
+print_r($db->showTables());
+
+if (!$db->ping()) echo 'Error connect' else echo 'Connected!';
---
+++ data/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+        'smi2/phpclickhouse' => array(
+            'pretty_version' => '1.4.6',
+            'version' => '1.4.6.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../smi2/phpclickhouse',
+            'aliases' => array(),
+            'reference' => '1c6f935e6fbea7e9935a1f9e76afa01bca0de5fe',
+            'dev_requirement' => false,
+        ),
+    ),
+);
